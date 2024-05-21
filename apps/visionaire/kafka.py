9c4a8dd7001fd7c7ff11859349bad994e33b5125
Brandon Harper <brandocarissian@gmail.com>
2024-05-21 16:35:18 -0600

    updated kafka manager to use confluent kafka

--- apps/visionaire/kafka.py
+++ apps/visionaire/kafka.py
@@ -1,22 +1,25 @@
 import json
-from kafka import KafkaProducer, KafkaConsumer, KafkaAdminClient
-from kafka.admin import NewTopic
-from kafka.errors import TopicAlreadyExistsError
 from datetime import datetime
+from confluent_kafka import Producer
+from confluent_kafka.admin import AdminClient, NewTopic
 
 def generate_hl7_audit_event(form_data, username, expiration_minutes):
-    current_time = datetime.now().isoformat() + 'Z'  # HL7 prefers timestamps in UTC with a 'Z' suffix
+    current_time = datetime.now().isoformat() + 'Z'
+    outcome_code = "success"
+    outcome_detail = "Authentication success"
     return {
         "resourceType": "AuditEvent",
         "type": {
             "system": "http://terminology.hl7.org/CodeSystem/audit-event-type",
-            "code": "110100",
-            "display": "User Authentication"
+            "code": "110126",
+            "display": "Authentication Decision"
         },
         "action": "E",
         "recorded": current_time,
-        "outcome": "0",  # Assuming success; use '4' or other appropriate codes for failures
-        "outcomeDesc": "Authentication success",
+        "outcome": {
+            "value": outcome_code,
+            "detail": outcome_detail
+        },
         "agent": [
             {
                 "who": {
@@ -101,9 +104,9 @@ def generate_hl7_audit_event(form_data, username, expiration_minutes):
         ]
     }
 
-
-
 class KafkaManager:
+    '''https://docs.confluent.io/kafka-clients/python/current/overview.html
+    '''
     def __init__(self, bootstrap_servers, client_id):
         self.bootstrap_servers = bootstrap_servers
         self.client_id = client_id
@@ -112,52 +115,41 @@ class KafkaManager:
 
     def get_producer(self):
         if not self.producer:
-            self.producer = KafkaProducer(
-                bootstrap_servers=self.bootstrap_servers,
-                value_serializer=lambda v: json.dumps(v).encode('utf-8')
-            )
+            self.producer = Producer({
+                'bootstrap.servers': self.bootstrap_servers,
+                'client.id': self.client_id
+            })
         return self.producer
 
     def get_admin_client(self):
         if not self.admin_client:
-            self.admin_client = KafkaAdminClient(
-                bootstrap_servers=self.bootstrap_servers,
-                client_id=self.client_id
-            )
+            self.admin_client = AdminClient({
+                'bootstrap.servers': self.bootstrap_servers,
+                'client.id': self.client_id
+            })
         return self.admin_client
 
     def create_topic(self, topic_name, num_partitions=1, replication_factor=1):
         admin_client = self.get_admin_client()
-        topics = admin_client.list_topics()
+        topics = admin_client.list_topics().topics
         if topic_name in topics:
             print(f"Topic '{topic_name}' already exists.")
             return False
         else:
-            topic_list = [NewTopic(name=topic_name, num_partitions=num_partitions, replication_factor=replication_factor)]
+            topic_list = [NewTopic(topic_name, num_partitions, replication_factor)]
             try:
-                admin_client.create_topics(new_topics=topic_list, validate_only=False)
+                admin_client.create_topics(topic_list)
                 print(f"Topic '{topic_name}' created successfully.")
                 return True
-            except TopicAlreadyExistsError:
-                print(f"Topic '{topic_name}' already exists.")
+            except Exception as e:
+                print(f"Failed to create topic '{topic_name}': {e}")
                 return False
-            
+
     def send_audit_event(self, topic, form_data, username, expiration_minutes):
         audit_event_json = generate_hl7_audit_event(form_data, username, expiration_minutes)
         self.send_message(topic, audit_event_json)
 
     def send_message(self, topic, message):
         producer = self.get_producer()
-        producer.send(topic, message)
+        producer.produce(topic, json.dumps(message).encode('utf-8'))
         producer.flush()
-
-    def consume_messages(self, topic, group_id):
-        consumer = KafkaConsumer(
-            topic,
-            bootstrap_servers=self.bootstrap_servers,
-            auto_offset_reset='earliest',
-            group_id=group_id,
-            value_deserializer=lambda m: json.loads(m.decode('utf-8'))
-        )
-        for message in consumer:
-            print(f"Received message: {message.value}")
